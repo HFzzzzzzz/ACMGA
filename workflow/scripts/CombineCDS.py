2445d389b6ebf3d078ba98d091274a4f42735218
790634750@qq.com <790634750@qq.com>
2023-12-27 21:50:55 +0800

    modify comment

--- workflow/scripts/CombineCDS.py
+++ workflow/scripts/CombineCDS.py
@@ -2,12 +2,12 @@
 from Bio import SeqIO
 import os
 seen_sequences = set()
-directory_path = './CDS'
+directory_path = './CDS' #Place cds file path
 
 all_files = os.listdir(directory_path)
 
-fasta_files = [file for file in all_files if file.endswith("fasta")]
-# print(fasta_files)
+fasta_files = [file for file in all_files if file.endswith("fasta")]#The suffix of cds file is .fasta
+
 with open("non_duplicate_CDS.fa", "w") as output_handle:
     for fasta_file in fasta_files:
         for record in SeqIO.parse(fasta_file, "fasta"):
