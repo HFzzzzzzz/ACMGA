a677dec9866e172c535a1d5f21977fab4e93adcc
“Zhang <zyxjh21@126.com>
2024-04-06 18:53:46 +0800

    ruff format replace_ref_que.py

--- workflow/scripts/replace_ref_que.py
+++ workflow/scripts/replace_ref_que.py
@@ -1,7 +1,6 @@
 import argparse
 
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input-file", required=True, help="input file")
 parser.add_argument("-o", "--output-file", required=True, help="output file")
@@ -10,20 +9,22 @@ parser.add_argument("-q", "--que", required=True, help="value of que")
 args = parser.parse_args()
 # with open(args.input_file) as input_file:
 
-with open(args.input_file, "r", encoding='utf-8') as input_file, open(args.output_file, "w", encoding='utf-8') as output_file:
+with open(args.input_file, "r", encoding="utf-8") as input_file, open(
+    args.output_file, "w", encoding="utf-8"
+) as output_file:
     for line in input_file:
         l = line.strip().split()  # noqa: E741
-        lineone=l[0]
-        linesix=l[5]
+        lineone = l[0]
+        linesix = l[5]
         # # Replace "chr10" at the beginning of the line with "id=foo|chr10".
         # Chr10 or chr10
-        if('chr' in lineone):
-            #chr 10
-            number = lineone.split('chr')[1]
+        if "chr" in lineone:
+            # chr 10
+            number = lineone.split("chr")[1]
             lineone_new = f"id={args.que}|chr{number}"
-        elif ('Chr' in lineone):
+        elif "Chr" in lineone:
             # Chr10
-            number = lineone.split('Chr')[1]
+            number = lineone.split("Chr")[1]
             lineone_new = f"id={args.que}|Chr{number}"
         else:
             print("Error: please check the chromosome name.")
@@ -33,11 +34,11 @@ with open(args.input_file, "r", encoding='utf-8') as input_file, open(args.outpu
             lineone_new = lineone
 
         # Replace "chr10" in the sixth column with "id=bar|chr10".
-        if('chr' in linesix):
-            number = linesix.split('chr')[1]
+        if "chr" in linesix:
+            number = linesix.split("chr")[1]
             linesix_new = f"id={args.ref}|chr{number}"
-        elif ('Chr' in linesix):
-            number = linesix.split('Chr')[1]
+        elif "Chr" in linesix:
+            number = linesix.split("Chr")[1]
             linesix_new = f"id={args.ref}|Chr{number}"
         else:
             print("Error: please check the chromosome name.")
